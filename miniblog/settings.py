23c188889b89c96c5329ff96dadc0807b2d00abc
alexladwong <alexladwong@gmail.com>
2024-04-27 19:12:17 +0300

    DeploymentEV02

--- miniblog/settings.py
+++ miniblog/settings.py
@@ -136,7 +136,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR/'media'
+STATIC_ROOT = BASE_DIR/'assets'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"

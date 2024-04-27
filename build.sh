7597e8fa537ee259a0f3c77f6ee495d4cdd20b56
alexladwong <alexladwong@gmail.com>
2024-04-27 17:37:18 +0300

    Deployment

---
+++ build.sh
@@ -0,0 +1,6 @@
+set -o errexit
+
+pip install r -requirements.txt
+
+python3 manage.py collectstatic --no-input
+python3 manage.py migrate 
--- miniblog/settings.py
+++ miniblog/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,6 +137,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 
 STATICFILES_DIRS = (
